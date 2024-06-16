ebbe5027e366a27cfbbc9db27b53f80d221072a3
Saikat-Goswami <thunder777saikat@gmail.com>
2024-06-16 22:11:46 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,14 @@
+# fruit =10
+# fruit1 =20
+# print("The fruit is: ", (fruit+fruit1))
+# s = "Prithwiraj khanki"
+# len_str = len(s)
+# print (len_str)
+# print (s[8])
+# s_new = s.replace("w","f")
+# print (s_new)
+# r = s[4:10]
+# print (r)
+name = input("write: ")
+age = input("write: ")
+# print ("hello",name,"!","my age",age,"!")
